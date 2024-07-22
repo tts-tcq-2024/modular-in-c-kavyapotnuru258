46945f522cfd51217cded27d488e2dda4bb4bee8
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-22 18:15:55 +0530

    Create NumberToColorPair.c

---
+++ NumberToColorPair.c
@@ -0,0 +1,14 @@
+// NumberToColorPair.c
+#include <stdio.h>
+#include "ColorPair.h"
+#include "ColorPairUtils.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
