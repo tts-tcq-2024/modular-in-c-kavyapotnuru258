d1bbf8c176deedc72213fe53a4b74403c3cb7b6c
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-22 18:13:58 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -15,8 +15,4 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-
 #endif // COLOR_PAIR_H
