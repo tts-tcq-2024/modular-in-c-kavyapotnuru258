1d7691b4afdcfca98670f678d12481e882df9e63
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-22 18:23:07 +0530

    Update ColorPairUtils.h

--- ColorPairUtils.h
+++ ColorPairUtils.h
@@ -1,11 +1,12 @@
-// ColorPairUtils.h
+// color_pair_utils.h
 #ifndef COLOR_PAIR_UTILS_H
 #define COLOR_PAIR_UTILS_H
 
-#include "ColorPair.h"
+#include "color_pair.h"
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
+void colorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair getColorFromPairNumber(int pairNumber);
+int getPairNumberFromColor(const ColorPair* colorPair);
+void printColorCodingReferenceManual();
 
 #endif // COLOR_PAIR_UTILS_H
