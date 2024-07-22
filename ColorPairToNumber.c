65ae4b2b155a832b5bc983c5c2baaa387d7601ab
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-22 18:23:24 +0530

    Update ColorPairToNumber.c

--- ColorPairToNumber.c
+++ ColorPairToNumber.c
@@ -1,26 +1,26 @@
-// ColorPairToNumber.c
+// color_pair_to_number.c
 #include <stdio.h>
-#include "ColorPair.h"
-#include "ColorPairUtils.h"
+#include "color_pair.h"
+#include "color_pair_utils.h"
 
-const char* MajorColorNames[] = {
+const char* majorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-const char* MinorColorNames[] = {
+const char* minorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+int numberOfMajorColors = sizeof(majorColorNames) / sizeof(majorColorNames[0]);
+int numberOfMinorColors = sizeof(minorColorNames) / sizeof(minorColorNames[0]);
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
+int getPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->major * numberOfMinorColors +
+            colorPair->minor + 1;
 }
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+void colorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+        majorColorNames[colorPair->major],
+        minorColorNames[colorPair->minor]);
 }
