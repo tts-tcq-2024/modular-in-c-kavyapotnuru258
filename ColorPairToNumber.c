50e7b39a60d0fd4eb1b6ee6fcac3548806da5fce
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-22 18:15:19 +0530

    Update and rename ColorPair.c to ColorPairToNumber.c

--- ColorPairToNumber.c
+++ ColorPairToNumber.c
@@ -1,6 +1,7 @@
-// ColorPair.c
+// ColorPairToNumber.c
 #include <stdio.h>
 #include "ColorPair.h"
+#include "ColorPairUtils.h"
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
@@ -13,23 +14,13 @@ const char* MinorColorNames[] = {
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
-}
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
 }
